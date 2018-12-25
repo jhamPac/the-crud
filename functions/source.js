5b23cd23aea9f11272e4e00e97f64a2f504609a9
jhamPac <cloud@kevia.me>
2018-12-24 22:50:05 -0700

    babelfying index.js and successfully serving it

--- functions/source.js
+++ functions/source.js
@@ -1,10 +1,11 @@
-import functions from 'firebase-functions'
-import express   from 'express'
+const functions = require("firebase-functions")
+const express   = require("express")
 
 // W.eb A.pplication F.ramework
 const WAF = express()
-  WAF.get('*', (request, response) => {
-    response.send('Merry Christmas and a happy New Year!')
-  })
+
+WAF.get('*', (request, response) => {
+  response.send('Merry Christmas and a happy New Year!')
+})
 
 export const api = functions.https.onRequest(WAF)
