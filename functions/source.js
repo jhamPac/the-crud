fbafb8df47478419569ba35a900289239f9f371e
jhamPac <cloud@kevia.me>
2018-12-24 23:11:33 -0700

    save point

--- functions/source.js
+++ functions/source.js
@@ -1,11 +1,20 @@
-const functions = require("firebase-functions")
-const express   = require("express")
+const functions = require('firebase-functions')
+const express   = require('express')
+const cors      = require('cors')
 
 // W.eb A.pplication F.ramework
 const WAF = express()
+
+WAF.use(cors({ origin: true }))
 
 WAF.get('*', (request, response) => {
   response.send('Merry Christmas and a happy New Year!')
 })
 
-export const api = functions.https.onRequest(WAF)
+export const api = functions.https.onRequest((request, response) => {
+  if (!request.path) {
+    request.url = `/${request.url}`
+  }
+
+  return WAF(request, response)
+})
