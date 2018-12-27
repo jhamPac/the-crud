13456035e8776d7abafadc6525f75debe7d54374
jhamPac <cloud@kevia.me>
2018-12-27 16:39:39 -0700

    found out what introspection key is

--- functions/source.js
+++ functions/source.js
@@ -1,6 +1,5 @@
 const admin     = require('firebase-admin')
 const functions = require('firebase-functions')
-const os        = require('os')
 
 // GraphQL
 import { ApolloServer }   from 'apollo-server-cloud-functions'
@@ -12,19 +11,7 @@ const gqlServer = new ApolloServer({
 })
 
 // FireBase setup
-function getCredentials() {
-  const serviceKeys = require(`${os.homedir()}/.firebase/keyfile.json`)
-  const adminCreds  = {
-    credential: admin.credential.cert(serviceKeys),
-    databaseURL: "https://blackjynxy.firebaseio.com"
-  }
-
-  return adminCreds
-}
-
-const fbConfig = (process.env.LOCAL_TEST) ? getCredentials() : functions.config().firebase
-admin.initializeApp(fbConfig)
-
+admin.initializeApp(functions.config().firebase)
 
 // export Firebase function handler
 export const api = functions.https.onRequest(gqlServer.createHandler({
