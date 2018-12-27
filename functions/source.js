fc72eb8e0778bf8504eab00c0707eb903358faf5
jhamPac <cloud@kevia.me>
2018-12-26 20:00:32 -0700

    added a way to locally test express routes

--- functions/source.js
+++ functions/source.js
@@ -1,6 +1,11 @@
+const admin     = require('firebase-admin')
 const functions = require('firebase-functions')
 const express   = require('express')
 const cors      = require('cors')
+
+// FireStore
+admin.initializeApp(functions.config().firebase)
+const DB = admin.firestore()
 
 // GraphQL deps
 const apolloServerExpress = require('apollo-server-express')
@@ -17,7 +22,19 @@ WAF.use((req, res, next) => {
 })
 
 WAF.get('/celebrate', (req, res) => {
-  res.send('Yaay Its Christmas!')
+  const foods = DB.collection('provisions').doc('food')
+  const getDoc = foods.get()
+  .then(doc => {
+    if (!doc.exists) {
+      console.log('No such document!');
+    } else {
+      console.log('Document data:', doc.data());
+    }
+  })
+  .catch(err => {
+    console.log('Error getting document', err);
+  });
+  res.send('Yaaaaay it was Christmas!')
 })
 
 WAF.get('/home', (req, res) => {
@@ -29,6 +46,10 @@ WAF.use('/', (req, res) => {
   res.set('Content-Type', 'text/plain')
   res.send('Merry Christmas and a Happy New Year')
 })
+
+if (process.env.LOCAL_TEST) {
+  WAF.listen(3000, () => console.log(`Example app listening on port 3000`))
+}
 
 export const api = functions.https.onRequest((req, res) => {
 	if (!req.path) {
