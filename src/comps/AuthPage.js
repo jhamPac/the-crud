8a49f79edee85804183c04b226b88ec7052efe8a
jhamPac <cloud@kevia.me>
2019-01-14 20:18:25 -0700

    trying to understand how Yup works

--- src/comps/AuthPage.js
+++ src/comps/AuthPage.js
@@ -1,36 +1,54 @@
 import React from 'react'
 import { firebaseRef } from '../firebase'
-import { Formik, FormikProps, Form, Field } from 'formik';
+import { Formik, FormikProps, Form, Field, ErrorMessage } from 'formik';
+import Yup form 'yup'
 
 export default function AuthPage(props) {
 
   function handleSubmit(values, { props, setSubmitting }) {
+    console.log(values)
     setSubmitting(false)
+    return
   }
 
   return (
     <div className="container">
-      <Formik
-        initialValues={{
-            email: '',
-            password: ''
-        }}
-        validate={(values) => {
-           let errors = [];
+      <section className="row">
+        <div className="col s12">
+          <Formik
+            initialValues={{
+                email: '',
+                password: ''
+            }}
+            validate={(values) => {
+               let errors = {};
 
-           return errors;
-        }}
-        onSubmit={handleSubmit}
-        render={(formProps) => {
-         return(
-            <Form>
-              <label htmlFor="email">Email</label>
-              <Field type="text" id="email" name="email"/>
-              <button className="btn btn-blue" type="submit" disabled={formProps.isSubmitting}>Submit Form</button>
-            </Form>
-         )
-       }}
-      />
+               if (!values.email) errors.email = "Email is required"
+
+               return errors;
+            }}
+            onSubmit={handleSubmit}
+            render={(formProps) => {
+             return(
+                <Form>
+                  <div>
+                    <label htmlFor="email">Email</label>
+                    <Field type="text" id="email" name="email"/>
+                    <ErrorMessage name="email" />
+                  </div>
+
+                  <div>
+                    <label htmlFor="password">Password</label>
+                    <Field type="password" id="password" name="password"/>
+                  </div>
+
+                  <button className="btn btn-blue" type="submit" disabled={formProps.isSubmitting}>Submit Form</button>
+                </Form>
+             )
+           }}
+          />
+        </div>
+      </section>
     </div>
   )
 }
