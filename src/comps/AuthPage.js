6da8b82c08d45e4562f5645398bc68ad5a05a906
jhamPac <cloud@kevia.me>
2019-01-14 20:08:15 -0700

    rough draft of Formik

--- src/comps/AuthPage.js
+++ src/comps/AuthPage.js
@@ -1,8 +1,36 @@
 import React from 'react'
 import { firebaseRef } from '../firebase'
+import { Formik, FormikProps, Form, Field } from 'formik';
 
-export default function AuthPage() {
+export default function AuthPage(props) {
+
+  function handleSubmit(values, { props, setSubmitting }) {
+    setSubmitting(false)
+  }
+
   return (
-    <div>This is the Authpage</div>
+    <div className="container">
+      <Formik
+        initialValues={{
+            email: '',
+            password: ''
+        }}
+        validate={(values) => {
+           let errors = [];
+
+           return errors;
+        }}
+        onSubmit={handleSubmit}
+        render={(formProps) => {
+         return(
+            <Form>
+              <label htmlFor="email">Email</label>
+              <Field type="text" id="email" name="email"/>
+              <button className="btn btn-blue" type="submit" disabled={formProps.isSubmitting}>Submit Form</button>
+            </Form>
+         )
+       }}
+      />
+    </div>
   )
 }
