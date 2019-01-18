06a8d04b10a020870c42a53e68a07058a4db829e
jhamPac <cloud@kevia.me>
2019-01-18 07:23:19 -0700

    user sign in and out

--- src/comps/Application.js
+++ src/comps/Application.js
@@ -1,21 +1,40 @@
-import React    from 'react'
+import React, { Component } from 'react'
 import { HashRouter as Router, Route, Switch } from 'react-router-dom'
+import { firebaseRef } from '../firebase'
 
 // comps
-import AuthPage   from './comps/AuthPage'
-import Dashboard  from './comps/Dashboard'
-import FoodSupply from './comps/FoodSupply'
+import AuthPage   from './AuthPage'
+import Dashboard  from './Dashboard'
+import FoodSupply from './FoodSupply'
 
-export default function Application() {
-  return(
-    <div id="UI">
-      <Router>
-        <Switch>
-          <Route path="/food-supply" component={ FoodSupply }></Route>
-          <Route path="/dashboard"   component={ Dashboard }></Route>
-          <Route exact path="/"      component={ AuthPage }></Route>
-        </Switch>
-      </Router>
-    </div>
-  )
+export default class Application extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      userLoggedIn: false
+    }
+
+    this.fireBaseListener = firebaseRef.auth().onAuthStateChanged(user => {
+      console.log(user, 'The user')
+      (user) ? this.setState({ userLoggedIn: true }) : this.setState({ userLoggedIn: false })
+    });
+  }
+
+  componentWillUnmount() {
+    this.fireBaseListener = undefined
+  }
+
+  render() {
+    return(
+      <div id="UI">
+        <Router>
+          <Switch>
+            <Route path="/food-supply" component={ FoodSupply }></Route>
+            <Route path="/dashboard"   component={ Dashboard }></Route>
+            <Route exact path="/"      component={ AuthPage }></Route>
+          </Switch>
+        </Router>
+      </div>
+    )
+  }
 }
--- src/comps/Dashboard.js
+++ src/comps/Dashboard.js
@@ -1,6 +1,12 @@
 import React from 'react'
+import { firebaseRef } from '../firebase'
 
 export default function Dashboard() {
+
+  setTimeout(async function() {
+    await firebaseRef.auth.signOut()
+  }, 8000)
+
   return (
     <div>
       <h1>Welcome!</h1>
