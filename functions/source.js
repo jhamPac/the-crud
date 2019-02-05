a7648e5569badef89a11e66231110d72a8f0e61e
jhamPac <cloud@kevia.me>
2019-02-04 20:35:58 -0700

    save before attempting to upgrade to babel 7

--- functions/source.js
+++ functions/source.js
@@ -1,8 +1,8 @@
 import { functions } from './firebaseSingleton'
 
 // GraphQL
-import { ApolloServer }   from 'apollo-server-cloud-functions'
-import schema             from './graphql/schema'
+import { ApolloServer } from 'apollo-server-cloud-functions'
+import schema           from './graphql/schema'
 
 const gqlServer = new ApolloServer({
   ...schema,
