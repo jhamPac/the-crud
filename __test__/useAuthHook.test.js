20da5f62f250caf185fdacca8117cb558dbb7824
jhamPac <cloud@kevia.me>
2019-02-12 08:08:53 -0700

    back to zero

--- __test__/useAuthHook.test.js
+++ __test__/useAuthHook.test.js
@@ -1,12 +1,12 @@
-import { useHook, cleanup } from 'react-hooks-testing-library'
+import { testHook, act, cleanup } from 'react-testing-library'
 import useAuth from 'hooks/useAuth'
 
 afterEach(cleanup)
 
 describe('useAuth hook', () => {
   test('initial value is false', () => {
-    const { getCurrentValue } = useHook(() => useAuth(false))
-    
+    const userLoggedIn = false
+
     expect(userLoggedIn).toBe(false)
   })
 })
