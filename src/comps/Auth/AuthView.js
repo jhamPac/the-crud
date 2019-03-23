16d11466299a3db282ecb5c6b786014934b80166
jhamPac <cloud@kevia.me>
2019-03-23 15:54:41 -0700

    removed spaces in rest of comps

--- src/comps/Auth/AuthView.js
+++ src/comps/Auth/AuthView.js
@@ -1,12 +1,11 @@
-import React, { Fragment } from 'react'
-import { firebaseRef }     from 'root/firebase'
-import M     from 'materialize-css'
+import React, {Fragment} from 'react'
+import {firebaseRef}     from 'root/firebase'
+import M from 'materialize-css'
 
 import LoginForm  from './LoginForm'
 import SignUpForm from './SignUpForm'
 
 export default function AuthView(props) {
-
   async function handleSignOut() {
     await firebaseRef.auth().signOut().catch(error => {
       M.toast({html: `${error.message}`})
@@ -24,11 +23,11 @@ export default function AuthView(props) {
 
           : <Fragment>
               <section className="login">
-                <LoginForm { ...props }/>
+                <LoginForm {...props}/>
               </section>
                <div className="divider"></div>
               <section className="sign-up">
-                <SignUpForm { ...props }/>
+                <SignUpForm {...props}/>
               </section>
             </Fragment>
       }
--- src/comps/Dashboard.js
+++ src/comps/Dashboard.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { firebaseRef } from '../firebase'
+import {firebaseRef} from '../firebase'
 
 export default function Dashboard() {
   return (
